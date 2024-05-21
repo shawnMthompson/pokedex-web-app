1c5ded4639abf60f8de9f126b301cbc4fd6e46e8
Shawn Thompson <sthompson7720@gmail.com>
2024-05-21 18:04:44 -0400

    Prettier Auto-Format + TailwindCSS testing line in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
+        <h1 className="font-bold text-pink-500 text-3x1 text-center mx-auto">
+          Testing Tailwind and Prettier
+        </h1>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -29,7 +32,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
