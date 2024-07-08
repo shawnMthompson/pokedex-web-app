4565b6b74894a9a907ec8f8f9157a66bc43f971d
Shawn Thompson <sthompson7720@gmail.com>
2024-07-08 19:39:20 -0400

    Added two more placeholder components—in which they detail their purpose for future reference.

---
+++ src/components/EducationTable.jsx
@@ -0,0 +1,5 @@
+/*  
+This .jsx (JavaScript XML) file will contain the JSX code for the EducationTable component. 
+The component will be a table that displays details regarding education, including the institution name,
+degree, and any relevant coursework.
+ */
---
+++ src/components/WorkTable.jsx
@@ -0,0 +1,5 @@
+/*  
+This .jsx (JavaScript XML) file will contain the JSX code for the WorkTable component. 
+The component will be a table that displays work experience, including the company name, job title,
+a description of the work done (in 3 bullet points, each a singular sentence), and the tech stack.
+ */
