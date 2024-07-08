e3df6950bd4eac9d049dc7254d4365cd7bc21939
Shawn Thompson <sthompson7720@gmail.com>
2024-07-08 19:29:51 -0400

    Added a placeholder component for the ProjectCard

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,5 @@
+/*  
+This .jsx (JavaScript XML) file will contain the JSX code for the ProjectCard component. 
+The component will be a card that displays the project name, description, and the tech stack, along with a link
+to the project's GitHub repository, a link to the live demo, and a button to view the project documentation. 
+ */
