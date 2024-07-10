310bc3788e0b2e2e03dad7a5876caf453182159c
Shawn Thompson <sthompson7720@gmail.com>
2024-07-10 13:41:52 -0400

    chore: Remove unused CSS classes and cleared the content in App.jsx, providing a basic test layout.

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import "./App.css"; // Importing CSS file
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <h1 className="text-3x1 mx-auto text-center font-bold text-pink-500">
-          Testing Tailwind and Prettier
-        </h1>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="text-blue-500" id="test">
+        <h1>Hello, World!</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
