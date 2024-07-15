6606c478b090bde00d2ec70daf1360979e2a800e
Shawn Thompson <sthompson7720@gmail.com>
2024-07-15 19:34:34 -0400

    chore: Updated project to a "pokedex-web-app", updating the title in package-lock.json and package.json, as well as removing any components and lines that are now unrelated to the current project.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import "./App.css"; // Importing CSS file
-import EducationTable from "./components/EducationTable"; // Importing EducationTable component
-import ProjectCard from "./components/ProjectCard"; // Importing ProjectCard component
-import WorkTable from "./components/WorkTable"; // Importing WorkTable component
 
 function App() {
   return (
@@ -9,9 +6,6 @@ function App() {
       <div className="p-10 text-blue-500" id="test">
         <h1>Hello, World!</h1>
       </div>
-      <EducationTable />
-      <ProjectCard />
-      <WorkTable />
     </>
   );
 }
