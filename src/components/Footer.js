901e4859d85566630559513b9ac39c64d7006bee
Shawn Thompson <sthompson7720@gmail.com>
2024-12-31 00:25:43 -0500

    Updated Footer layout for improved structure and linked the Header component to the main page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,40 +2,35 @@ import { FaRegCopyright } from "react-icons/fa";
 import Image from "next/image";
 
 export default function Footer() {
-  // Retrieve Current Year
   const currentDate = new Date();
   const year = currentDate.getFullYear();
 
   return (
-    <div className="bg-gray-500 text-white font-bold p-4 flex justify-evenly ">
-      <div className="flex items-center">
-        <FaRegCopyright />
-        <p className="ml-1">PokéIndex {year}</p>
+    <div className="bg-gray-500 text-white font-bold p-4 flex flex-col items-center">
+      <div className="w-full flex justify-between items-center">
+        <div className="flex items-center">
+          <FaRegCopyright />
+          <p className="ml-1">PokéIndex {year}</p>
+        </div>
+        <div className="flex items-center">
+          <p className="mr-1">Powered By</p>
+          <a target="_blank" href="https://pokeapi.co">
+            <Image src="/pokeAPI.png" alt="pokeAPI" width={60} height={20} />
+          </a>
+        </div>
       </div>
-      <div className="flex flex-col items-center max-w-100 text-center text-sm">
+      <div className="flex flex-col items-center text-center text-sm">
         <Image
           src="/pokeball.png"
           alt="pokeballIcon"
           width={54}
           height={48}
           className="mb-2"
-        ></Image>
-        <p className="">
+        />
+        <p>
           All Pokémon content and materials are trademarks and copyrights of
           Nintendo and Game Freak.
         </p>
-      </div>
-      <div className="flex items-center">
-        <p className="mr-1">Powered By</p>
-        <a target="_blank" href="https://pokeapi.co">
-          <Image
-            src="/pokeAPI.png"
-            alt="pokeAPI"
-            width={60}
-            height={48}
-            className="ml-1"
-          ></Image>
-        </a>
       </div>
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div>
+      <h1 className="text-white text-center text-7xl">PokéIndex</h1>
+    </div>
+  );
+}
