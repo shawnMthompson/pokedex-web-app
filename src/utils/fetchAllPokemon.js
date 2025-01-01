6079a6b92901b220127e74ec8c64ca9b2d7f1175
Shawn Thompson <sthompson7720@gmail.com>
2024-12-31 21:42:01 -0500

    Refactored fetchAllPokemon and fetchAndFormatAllPokemon functions to use the fetchPokemon utility for improved code clarity and maintainability

--- src/utils/fetchAllPokemon.js
+++ src/utils/fetchAllPokemon.js
@@ -1,23 +1,26 @@
-import { formatPokemon } from '@/utils/formatData';
+import { formatPokemon } from "@/utils/formatData";
+import { fetchPokemon } from "@/utils/fetchPokemon";
 
 const baseURL = "https://pokeapi.co/api/v2";
+
+// Fetch a list of pokemon with basic information (their names)
 export async function fetchAllPokemon(limit) {
-    const response = await fetch(`${baseURL}/pokemon?limit=${limit}`);
-    const data = await response.json();
-    return data.results;
+  const response = await fetch(`${baseURL}/pokemon?limit=${limit}`);
+  const data = await response.json();
+  return data.results;
 }
 
+// Fetches detailed information of pokemon in the list and formats it
 export async function fetchAndFormatAllPokemon(limit) {
-    const pokemonList = await fetchAllPokemon(limit)
+  const pokemonList = await fetchAllPokemon(limit);
 
-    const formattedPokemonList = await Promise.all(
-        pokemonList.map(async (pokemon) => {
-            const response = await fetch(`${baseURL}/pokemon/${pokemon.name}`);
-            const rawData = await response.json();
-            const formattedData = formatPokemon(rawData);
-            return formattedData;
-        })
-    );
+  const formattedPokemonList = await Promise.all(
+    pokemonList.map(async (pokemon) => {
+      const rawData = await fetchPokemon(pokemon.name);
+      const formattedData = formatPokemon(rawData);
+      return formattedData;
+    })
+  );
 
-    return formattedPokemonList;
+  return formattedPokemonList;
 }
