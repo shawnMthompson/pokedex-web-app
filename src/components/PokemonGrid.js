172d392e04223345c591ae6aed6d6c478e712ef9
Shawn Thompson <sthompson7720@gmail.com>
2025-01-12 18:26:52 -0500

    Added infinite scroll functionality to PokemonGrid component and updated fetch functions for pagination to allow for it

--- src/components/PokemonGrid.js
+++ src/components/PokemonGrid.js
@@ -1,18 +1,72 @@
+"use client";
+
+import { useState, useEffect, useCallback } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchAndFormatAllPokemon } from "@/utils/fetchAllPokemon";
 import PokemonCard from "./PokemonCard";
 
-// More responsive work needs to be done here still.
+export default function PokemonGrid() {
+  const [pokemonList, setPokemonList] = useState([]);
+  const [hasMore, setHasMore] = useState(true);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(true);
+  const limit = 36;
 
-export default async function PokemonGrid() {
-  const pokemonList = await fetchAndFormatAllPokemon(18);
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      const offset = (page - 1) * limit;
+      const data = await fetchAndFormatAllPokemon(limit, offset);
+      setPokemonList((prevList) => [...prevList, ...data]);
+      setLoading(false);
+      if (data.length === 0 || data.length < limit) {
+        setHasMore(false);
+      }
+    };
+
+    fetchData();
+  }, [page]);
+
+  const fetchMoreData = useCallback(() => {
+    if (hasMore) {
+      setPage((prevPage) => prevPage + 1);
+    }
+  }, [hasMore]);
 
   return (
-    <div className="container mx-auto p-4 flex">
-      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 p-4">
-        {pokemonList.map((pokemon) => (
-          <PokemonCard key={pokemon.id} pokemon={pokemon} /> // Create PokemonCard components with individual pokemon props
-        ))}
-      </div>
+    <div className="container mx-auto p-4">
+      {loading && page === 1 ? (
+        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 p-4">
+          {Array.from({ length: 36 }).map((_, index) => (
+            <div
+              key={`loader-${index}`}
+              className="bg-gray-200 animate-pulse h-64 w-full rounded-lg"
+            ></div>
+          ))}
+        </div>
+      ) : (
+        <InfiniteScroll
+          dataLength={pokemonList.length}
+          next={fetchMoreData}
+          hasMore={hasMore}
+          loader={
+            <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 p-4">
+              {Array.from({ length: 36 }).map((_, index) => (
+                <div
+                  key={`loader-${index}`}
+                  className="bg-gray-200 animate-pulse h-64 w-full rounded-lg"
+                ></div>
+              ))}
+            </div>
+          }
+        >
+          <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 p-4">
+            {pokemonList.map((pokemon) => (
+              <PokemonCard key={pokemon.id} pokemon={pokemon} />
+            ))}
+          </div>
+        </InfiniteScroll>
+      )}
     </div>
   );
 }
--- src/utils/fetchAllPokemon.js
+++ src/utils/fetchAllPokemon.js
@@ -2,17 +2,20 @@ import { formatPokemon } from "@/utils/formatData";
 import { fetchPokemon } from "@/utils/fetchPokemon";
 
 const baseURL = "https://pokeapi.co/api/v2";
+const MAX_POKEMON_ID = 1025;
 
 // Fetch a list of pokemon with basic information (their names)
-export async function fetchAllPokemon(limit) {
-  const response = await fetch(`${baseURL}/pokemon?limit=${limit}`);
+export async function fetchAllPokemon(limit, offset) {
+  const response = await fetch(
+    `${baseURL}/pokemon?limit=${limit}&offset=${offset}`
+  );
   const data = await response.json();
   return data.results;
 }
 
 // Fetches detailed information of pokemon in the list and formats it
-export async function fetchAndFormatAllPokemon(limit) {
-  const pokemonList = await fetchAllPokemon(limit);
+export async function fetchAndFormatAllPokemon(limit, offset) {
+  const pokemonList = await fetchAllPokemon(limit, offset);
 
   const formattedPokemonList = await Promise.all(
     pokemonList.map(async (pokemon) => {
@@ -22,5 +25,5 @@ export async function fetchAndFormatAllPokemon(limit) {
     })
   );
 
-  return formattedPokemonList;
+  return formattedPokemonList.filter((pokemon) => pokemon.id <= MAX_POKEMON_ID);
 }
