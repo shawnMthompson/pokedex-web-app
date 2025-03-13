ac7e74a788308aa3ba4216de5f00e0690e9a00a8
Shawn Thompson <sthompson7720@gmail.com>
2025-03-12 21:39:02 -0400

    Refactored hover effect to the PokemonCard component

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,15 +2,20 @@
 
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function PokemonCard({ pokemon }) {
   const router = useRouter();
   const handleClick = () =>
     router.push(`/pokemon/${pokemon.name.toLowerCase()}`);
 
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
     <div
       onClick={handleClick}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
       className="bg-cardBase shadow-md hover:shadow-lg rounded-lg overflow-hidden text-gray-500 cursor-pointer transition-shadow duration-300 hover:jiggle"
     >
       <Image
@@ -19,7 +24,7 @@ export default function PokemonCard({ pokemon }) {
         height={256}
         width={256}
         priority={true}
-        className="p-4 hover:up-and-down"
+        className={`${isHovered ? "up-and-down" : ""} p-4`}
       />
       <div className="p-4">
         <h3 className="text-xl font-bold text-foreground">#{pokemon.id}</h3>
